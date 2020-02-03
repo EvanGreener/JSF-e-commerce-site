faf3eee6ddb828611fee227f6375f6cccf906d3a
Evan Greenstein <evanlg16@gmail.com>
2020-02-03 14:42:39 -0500

    Semantic UI integration successful! Also tweaked the pom file to the correct stuff

--- book_store_project/src/main/java/com/gb1w20/book_store_project/JAXRSConfiguration.java
+++ book_store_project/src/main/java/com/gb1w20/book_store_project/JAXRSConfiguration.java
@@ -1,13 +1,13 @@
-package com.gb1w20.book_store_project;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-/**
- * Configures JAX-RS for the application.
- * @author Juneau
- */
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-    
-}
+//package com.gb1w20.book_store_project;
+//
+//import javax.ws.rs.ApplicationPath;
+//import javax.ws.rs.core.Application;
+//
+///**
+// * Configures JAX-RS for the application.
+// * @author Juneau
+// */
+//@ApplicationPath("resources")
+//public class JAXRSConfiguration extends Application {
+//    
+//}
