c583750351eac23f6560f5145d0dd9ffffe9898c
shrutipareek <06spareek@gmail.com>
2020-02-13 08:01:12 -0500

    added hover buttons to all books

--- book_store_project/target/book_store_project-1.0-SNAPSHOT/resources/js/homepage.js
+++ book_store_project/target/book_store_project-1.0-SNAPSHOT/resources/js/homepage.js
@@ -1,7 +1,20 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+document.addEventListener('DOMContentLoaded', function(){
+    var bookCards=document.getElementsByClassName('image');
+    var index;
+for(index=0;index<bookCards.length;index++){
+    bookCards[index].addEventListener('mouseover',displayCardHoverButton); bookCards[index].addEventListener('mouseout',removeCardHoverButton);
+}});
+function displayCardHoverButton()
+{
+   var btn = document.createElement("BUTTON");
+   
+   //TODO: put into message bundle for localization
+    btn.innerHTML = "Add To Cart";
+    btn.classList.add("cardHoverBtn");
+ this.appendChild(btn);
+}
+function removeCardHoverButton()
+{
+   var btn = document.getElementsByClassName("cardHoverBtn");
+  btn[0].remove();
+}
