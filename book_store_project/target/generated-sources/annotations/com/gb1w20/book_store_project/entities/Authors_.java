a7d0931699a74dc198abd1fd06b06312d2d427c6
Giancarlo Biasiucci <giancarlo.biasiucci@dawsoncollege.qc.ca>
2020-02-13 15:51:11 -0500

    fix connection to database

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Authors_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Authors_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-02-13T13:11:46", comments="EclipseLink-2.7.5.v20191016-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-02-13T15:48:41", comments="EclipseLink-2.7.6.v20200131-rNA")
 @StaticMetamodel(Authors.class)
 public class Authors_ { 
 
