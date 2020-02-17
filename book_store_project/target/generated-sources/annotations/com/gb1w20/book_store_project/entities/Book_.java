e95a70b31ac556ba4ea261137e3889bcecec3822
Evan Greenstein <evanlg16@gmail.com>
2020-02-17 15:21:05 -0500

    Worksgit add .git add .git add .

---
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Book_.java
@@ -0,0 +1,27 @@
+package com.gb1w20.book_store_project.entities;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-02-17T15:17:30", comments="EclipseLink-2.7.6.v20200131-rNA")
+@StaticMetamodel(Book.class)
+public class Book_ { 
+
+    public static volatile SingularAttribute<Book, Integer> numOfPages;
+    public static volatile SingularAttribute<Book, BigDecimal> salePrice;
+    public static volatile SingularAttribute<Book, String> isbn;
+    public static volatile SingularAttribute<Book, String> description;
+    public static volatile SingularAttribute<Book, String> title;
+    public static volatile SingularAttribute<Book, Date> dateOfPublication;
+    public static volatile SingularAttribute<Book, Date> dateEntered;
+    public static volatile SingularAttribute<Book, Integer> publisherID;
+    public static volatile SingularAttribute<Book, Boolean> isRemoved;
+    public static volatile SingularAttribute<Book, String> genre;
+    public static volatile SingularAttribute<Book, Date> lastModified;
+    public static volatile SingularAttribute<Book, BigDecimal> wholesalePrice;
+    public static volatile SingularAttribute<Book, BigDecimal> listPrice;
+
+}
