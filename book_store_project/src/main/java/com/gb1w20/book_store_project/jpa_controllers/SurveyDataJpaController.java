389e3c37a88eae3d869f5904ad4f1a815f85d042
Giancarlo Biasiucci <giancarlo.biasiucci@dawsoncollege.qc.ca>
2020-02-17 14:07:58 -0500

    Fixed entity named annotations

--- book_store_project/src/main/java/com/gb1w20/book_store_project/jpa_controllers/SurveyDataJpaController.java
+++ book_store_project/src/main/java/com/gb1w20/book_store_project/jpa_controllers/SurveyDataJpaController.java
@@ -20,7 +20,7 @@ import javax.transaction.UserTransaction;
  *
  * @author Saad
  */
-@Named("surveyData")
+@Named
 @RequestScoped
 public class SurveyDataJpaController implements Serializable {
     
--- book_store_project/src/main/java/com/gb1w20/book_store_project/jpa_controllers/TaxJpaController.java
+++ book_store_project/src/main/java/com/gb1w20/book_store_project/jpa_controllers/TaxJpaController.java
@@ -20,7 +20,7 @@ import javax.transaction.UserTransaction;
  *
  * @author Saad
  */
-@Named("tax")
+@Named
 @RequestScoped
 public class TaxJpaController implements Serializable {
     
