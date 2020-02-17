b4b9e78fbd9fa556561cb3de018ddff368f1f35b
Shruti Pareek <shruti.pareek@dawsoncollege.qc.ca>
2020-02-17 13:58:22 -0500

    can access data from database

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Book_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Book_.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-02-13T15:48:41", comments="EclipseLink-2.7.6.v20200131-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-02-17T13:55:35", comments="EclipseLink-2.7.6.v20200131-rNA")
 @StaticMetamodel(Book.class)
 public class Book_ { 
 
