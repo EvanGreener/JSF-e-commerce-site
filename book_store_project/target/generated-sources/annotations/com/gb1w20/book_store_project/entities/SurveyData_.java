7f200f1e59c4b6754199e59596a7fd4a51ee1e01
Giancarlo Biasiucci <giancarlo.biasiucci@dawsoncollege.qc.ca>
2020-02-17 15:55:11 -0500

    began work on sign in page

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/SurveyData_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/SurveyData_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-02-17T13:55:35", comments="EclipseLink-2.7.6.v20200131-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-02-17T15:18:00", comments="EclipseLink-2.7.6.v20200131-rNA")
 @StaticMetamodel(SurveyData.class)
 public class SurveyData_ { 
 
