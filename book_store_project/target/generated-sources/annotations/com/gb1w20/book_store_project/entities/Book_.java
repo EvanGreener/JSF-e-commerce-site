77227195992bb3ac7a54543fbf49eca8387c7c32
Giancarlo Biasiucci <giancarlo.biasiucci@dawsoncollege.qc.ca>
2020-02-20 14:41:05 -0500

    Finished the code and layout for the login and register pages

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Book_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Book_.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-02-19T14:27:33", comments="EclipseLink-2.7.6.v20200131-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-02-20T14:20:32", comments="EclipseLink-2.7.6.v20200131-rNA")
 @StaticMetamodel(Book.class)
 public class Book_ { 
 
