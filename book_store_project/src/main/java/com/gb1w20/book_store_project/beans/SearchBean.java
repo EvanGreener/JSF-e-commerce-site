4c0609adf16372e0e97eb8b0dcae301eb8f66b70
Evan Greenstein <evanlg16@gmail.com>
2020-02-20 23:23:22 -0500

    Pagination resizng now works on page reload and on keyup

--- book_store_project/src/main/java/com/gb1w20/book_store_project/beans/SearchBean.java
+++ book_store_project/src/main/java/com/gb1w20/book_store_project/beans/SearchBean.java
@@ -4,9 +4,13 @@ import com.gb1w20.book_store_project.entities.Book;
 import com.gb1w20.book_store_project.jpa_controllers.BookJpaController;
 import java.io.Serializable;
 import java.util.List;
+import javax.annotation.PostConstruct;
 
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.event.ComponentSystemEvent;
 import javax.inject.Inject;
 
 @Named("search")
@@ -16,11 +20,21 @@ public class SearchBean implements Serializable {
     @Inject
     private BookJpaController bookCtrlr;
 
-    private String query = "";
-    private String[] genreFilters = {"All"};
-    private String searchBy = "";
+    private String query;
+    private String[] genreFilters;
+    private String searchBy;
+    private List<Book> results;
     private int page = 1;
-    private int numPages = 1;
+    private int numPages;
+
+    @PostConstruct
+    public void init() {
+        page = 1;
+        query = "";
+        results = !query.isBlank() ? bookCtrlr.search(query, page) : bookCtrlr.findBookEntities();
+        numPages = (int) Math.ceil(results.size() / 8.0);
+    }
+
 
     public String[] getGenreFilters() {
         return genreFilters;
@@ -63,11 +77,11 @@ public class SearchBean implements Serializable {
     }
 
     public List<Book> getResults() {
-        List<Book> res = !query.isBlank() ? bookCtrlr.search(query, page) : bookCtrlr.findBookEntities();
-        numPages = (int) Math.ceil(res.size() / 8.0);
-        System.out.println("Num pages - " + numPages);
-        System.out.println("Page # - " + page + "\n");
+        return results;
+    }
 
-        return res;
+    public void onKeyUp(AjaxBehaviorEvent event) {
+        results = !query.isBlank() ? bookCtrlr.search(query, page) : bookCtrlr.findBookEntities();
+        numPages = (int) Math.ceil(results.size() / 8.0);
     }
 }
