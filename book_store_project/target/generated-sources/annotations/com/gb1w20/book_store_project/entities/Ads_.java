369b3867d8eac30a9b1538f55a8243fbe9e54d21
Giancarlo Biasiucci <giancarlo.biasiucci@dawsoncollege.qc.ca>
2020-03-05 18:38:57 -0500

    Finished removal functionality for managerial order page

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Ads_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Ads_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-05T15:53:44")
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-05T18:24:03")
 @StaticMetamodel(Ads.class)
 public class Ads_ { 
 
--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/OrderItem_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/OrderItem_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-05T15:53:44")
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-05T18:24:03")
 @StaticMetamodel(OrderItem.class)
 public class OrderItem_ { 
 
