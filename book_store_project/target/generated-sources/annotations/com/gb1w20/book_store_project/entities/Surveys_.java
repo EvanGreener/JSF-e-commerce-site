e373e0eff05020a119f5cfff016e3fe56243fb66
Giancarlo Biasiucci <giancarlo.biasiucci@dawsoncollege.qc.ca>
2020-03-05 13:37:51 -0500

    Merging with Cedric's branch

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Surveys_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Surveys_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2020-03-05T09:40:20")
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-05T13:36:42")
 @StaticMetamodel(Surveys.class)
 public class Surveys_ { 
 
