11f9ab955755bc6b8189d5e2e7314cd2edd4607f
Evan Greenstein <evanlg16@gmail.com>
2020-03-05 13:55:07 -0500

    Actually fixed conflicts

---
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Surveys_.java
@@ -0,0 +1,19 @@
+package com.gb1w20.book_store_project.entities;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2020-03-05T13:48:36")
+@StaticMetamodel(Surveys.class)
+public class Surveys_ { 
+
+    public static volatile SingularAttribute<Surveys, Integer> surveyID;
+    public static volatile SingularAttribute<Surveys, String> surveyDescription;
+    public static volatile SingularAttribute<Surveys, Date> dateCreated;
+    public static volatile SingularAttribute<Surveys, Boolean> isRemoved;
+    public static volatile SingularAttribute<Surveys, Date> lastModified;
+    public static volatile SingularAttribute<Surveys, String> surveryName;
+
+}
