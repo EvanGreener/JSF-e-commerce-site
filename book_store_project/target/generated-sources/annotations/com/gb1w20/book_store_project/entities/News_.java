efaf239c31d748e47182cb1925b167e7871afb69
Evan Greenstein <evanlg16@gmail.com>
2020-03-11 22:19:07 -0400

    Modals not centered and only updates after two clicks

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/News_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/News_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2020-03-10T23:00:12")
+@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2020-03-11T21:52:44")
 @StaticMetamodel(News.class)
 public class News_ { 
 
