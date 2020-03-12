e815dd2c9918ade64cd2de255adbbc5f591da315
Saad Khan <saaadkhan23@yahoo.ca>
2020-03-12 15:14:27 -0400

    small changes

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Publisher_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Publisher_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2020-03-11T05:38:10")
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-11T17:43:01")
 @StaticMetamodel(Publisher.class)
 public class Publisher_ { 
 
