e027ab0dbc4d63bd616cbef29d06db13f8a450bd
Giancarlo Biasiucci <giancarlo@MyMac.local>
2020-03-21 15:23:03 -0400

    Working on adding help tags to each client page

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Book_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Book_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-20T20:11:51")
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-21T14:08:57")
 @StaticMetamodel(Book.class)
 public class Book_ { 
 
