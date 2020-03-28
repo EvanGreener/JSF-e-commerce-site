e7cabbf70c4f05c3cc26e06f22896074d55ab860
Cedric Richards <cawrichards@gmail.com>
2020-03-28 18:52:44 -0400

    preparing to pull

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Authors_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Authors_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2020-03-20T16:22:08")
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-28T18:51:12")
 @StaticMetamodel(Authors.class)
 public class Authors_ { 
 
