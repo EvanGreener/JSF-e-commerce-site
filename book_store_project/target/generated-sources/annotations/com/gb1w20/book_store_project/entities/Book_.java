9b7442d2e2fc72f90a3b18e6b979ab48566bc3fd
Giancarlo Biasiucci <giancarlo@MyMac.local>
2020-04-01 11:13:12 -0400

    Performed Arquillian testing, tested BookFormat

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Book_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Book_.java
@@ -12,7 +12,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-31T15:45:31")
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-04-01T11:11:16")
 @StaticMetamodel(Book.class)
 public class Book_ { 
 
--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/OrderItem_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/OrderItem_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-03-31T15:45:31")
+@Generated(value="EclipseLink-2.7.6.v20200131-rNA", date="2020-04-01T11:11:16")
 @StaticMetamodel(OrderItem.class)
 public class OrderItem_ { 
 
