856cee23370b6c601a9e3319058f6d91bdb7d0c0
Evan Greenstein <evanlg16@gmail.com>
2020-04-10 22:43:46 -0400

    blah

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Ads_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Ads_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-04-10T21:21:01", comments="EclipseLink-2.7.6.v20200131-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-04-10T22:40:09", comments="EclipseLink-2.7.6.v20200131-rNA")
 @StaticMetamodel(Ads.class)
 public class Ads_ { 
 
