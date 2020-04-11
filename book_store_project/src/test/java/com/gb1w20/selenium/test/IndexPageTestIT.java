a3e9f567e87bf4989100d1622750d519799fe863
Saad Khan <saaadkhan23@yahoo.ca>
2020-04-11 00:31:34 -0400

    create test headers

--- book_store_project/src/test/java/com/gb1w20/selenium/test/IndexPageTestIT.java
+++ book_store_project/src/test/java/com/gb1w20/selenium/test/IndexPageTestIT.java
@@ -1,7 +1,9 @@
 package com.gb1w20.selenium.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.List;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,11 +16,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 /**
  * Selenium test class for the client front page
+ *
  * @author Giancarlo Biasiucci, Saad Khan
  * @version April 6, 2020
  */
 public class IndexPageTestIT {
-    
+
     private WebDriver driver;
 
     @BeforeClass
@@ -28,18 +31,18 @@ public class IndexPageTestIT {
         // downloads the required driver and makes it available
         WebDriverManager.chromedriver().setup();
     }
-    
+
     @Before
     public void setupTest() {
         driver = new ChromeDriver();
     }
-    
+
     @After
     public void shutdownTest() {
         //Close the browser
         driver.quit();
     }
-    
+
     @Test
     public void testIndexTitle() throws Exception {
         // And now use this to visit a web site
@@ -49,7 +52,7 @@ public class IndexPageTestIT {
         // Wait for the page to load, timeout after 10 seconds
         wait.until(ExpectedConditions.titleIs("The BookStore"));
     }
-    
+
     @Test
     public void testIndexSearch() throws Exception {
         // And now use this to visit a web site
@@ -61,9 +64,67 @@ public class IndexPageTestIT {
         WebElement searchBar = driver.findElement(By.className("searchBar"));
         searchBar.clear();
         searchBar.sendKeys("test");
-        
+
         WebElement searchButton = driver.findElement(By.className("searchButton"));
         searchButton.click();
         wait.until(ExpectedConditions.titleIs("Gallery Page"));
     }
+
+    @Test
+    public void testItemSliderHasAllItems() throws Exception {
+        // And now use this to visit a web site
+        driver.get("http://localhost:8080/book_store_project/");
+        // Wait for the page to load, timeout after 10 seconds
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        // Wait for the page to load, timeout after 10 seconds
+        wait.until(ExpectedConditions.titleIs("The BookStore"));
+        List<WebElement> sliderItems = driver.findElements(By.className("swiper-slide"));
+        Assert.assertEquals(5, sliderItems.size());
+    }
+    
+    @Test
+    public void testShopNowButon() throws Exception{
+                // And now use this to visit a web site
+        driver.get("http://localhost:8080/book_store_project/");
+        // Wait for the page to load, timeout after 10 seconds
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        // Wait for the page to load, timeout after 10 seconds
+        wait.until(ExpectedConditions.titleIs("The BookStore"));
+        List<WebElement> swiperItemBtn = driver.findElements(By.className("swiperButton"));
+        swiperItemBtn.get(0).click();
+        wait.until(ExpectedConditions.urlContains("book.xhtml?isbn="));
+    }
+    
+    @Test
+    public void testBookViewCover() throws Exception{
+    }
+    
+    @Test
+    public void testBookTitleClick() throws Exception{
+    }
+    
+    @Test
+    public void testAddBookToCart() throws Exception{
+    }
+    
+    @Test
+    public void testSurveyOptionSelect() throws Exception{
+    }
+    
+    @Test
+    public void testSurveyOptionSelectAndSubmit() throws Exception{
+    }
+    
+    @Test
+    public void testPopularGenre() throws Exception{
+    }
+    
+    @Test
+    public void testAddClick() throws Exception{
+    }
+    
+    @Test
+    public void testHelpPageClick() throws Exception{
+    }           
+            
 }
--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Publisher_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Publisher_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-04-10T10:58:01", comments="EclipseLink-2.7.6.v20200131-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-04-10T17:33:52", comments="EclipseLink-2.7.6.v20200131-rNA")
 @StaticMetamodel(Publisher.class)
 public class Publisher_ { 
 
