6841907e897a148b6777f825027af0d882e4d776
Evan Greenstein <evanlg16@gmail.com>
2020-04-11 14:24:19 -0400

    Fixed it

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Tax_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Tax_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-04-10T23:38:01", comments="EclipseLink-2.7.6.v20200131-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-04-11T14:10:32", comments="EclipseLink-2.7.6.v20200131-rNA")
 @StaticMetamodel(Tax.class)
 public class Tax_ { 
 
