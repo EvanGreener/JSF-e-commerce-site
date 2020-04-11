4eb83e2c6b19bed420e92e93928e451a006b3bd2
shrutipareek <06spareek@gmail.com>
2020-04-10 23:43:50 -0400

    fixed a bug in manager inventory when editing

--- book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Orders_.java
+++ book_store_project/target/generated-sources/annotations/com/gb1w20/book_store_project/entities/Orders_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-04-10T20:20:37", comments="EclipseLink-2.7.6.v20200131-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2020-04-10T21:46:57", comments="EclipseLink-2.7.6.v20200131-rNA")
 @StaticMetamodel(Orders.class)
 public class Orders_ { 
 
